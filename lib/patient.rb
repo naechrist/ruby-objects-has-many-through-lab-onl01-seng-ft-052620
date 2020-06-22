a2d77616daa9087325764efe38198369b8535211
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-22 01:52:37 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def doctors
-    Appointment.collect {|appointment| appointment.doctor}
+    appointments.collect {|appointment| appointment.doctor}
   end
 
   def new_appointment(doctor, date)
