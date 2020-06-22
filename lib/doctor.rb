ceb22c415e9a9708b4b344d092a4d89bfbf6bc89
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-22 01:58:25 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def patients
-    Appointment.all.collect {|appointment| appointment.doctor }
+    Appointment.all.map {|appointment| appointment.doctor }
   end
    
   # def patients
